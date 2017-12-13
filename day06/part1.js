7f3432979c9fb0b471e64b4da05a11c7b329de65
hyber1z0r <jakobsky@live.dk>
2017-12-13 19:10:56 +0100

    feat(Day06): Add solutions for part1 and part2

---
+++ day06/part1.js
@@ -0,0 +1,37 @@
+'use strict';
+
+module.exports = function solve(input) {
+  const banks = input.split(' ').map(Number);
+  const hashes = [];
+
+  hashes[banks.join(',')] = true;
+
+  let cycles = 0;
+
+  while (true) {
+      // Find the highest value of the banks (7, index 2)
+      let highest = Math.max(...banks);
+
+      // Set the value to 0 in the array
+      let highestIndex = banks.indexOf(highest);
+      banks[highestIndex] = 0;
+
+      // Redistribute all values from highestBank
+      while (highest > 0) {
+        highestIndex = (highestIndex + 1) % banks.length;
+        banks[highestIndex]++;
+        highest--;
+      }
+
+      cycles++;
+
+      // Check for infinite loop
+      const hash = banks.join(',');
+      if (hashes[hash]) break;
+
+      hashes[hash] = true;
+  }
+
+  return cycles;
+};
+
--- puzzlerunner.js
+++ puzzlerunner.js
@@ -1,9 +1,9 @@
 'use strict';
 const InputLoader = require('./InputLoader');
-const solve = require('./day05/part2');
+const solve = require('./day06/part2');
 
 (async () => {
-  const input = await new InputLoader('day05').loadInput();
+  const input = await new InputLoader('day06').loadInput();
   const output = solve(input);
   console.log('answer', output);
 })();
